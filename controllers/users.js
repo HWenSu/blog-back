67b755aa36644c2193b7d6bd00f2304ee98fcc8d
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-13 11:39:57 +0800

    upload image

--- controllers/users.js
+++ controllers/users.js
@@ -54,10 +54,11 @@ class UsersController {
     })
   }
 
-  async logout(req, res) {
+  async upload(req, res) {
     const token = req.headers.authorization.split(' ')[1]
-    invalidatedTokens.push(token)
-    res.sendStatus(200)
+    console.log(token)
+    console.log(req.file)
+    res.send(req.file)
   }
 
   verifyToken(req, res, next) {
--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,11 +1,26 @@
 const { Router } = require('express')
 const router = Router()
 
+const timestamp = require('../../utils')
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, timestamp() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage })
+
 const { usersController } = require('../../controllers')
 
 router.post('/login', usersController.login)
 router.post('/register', usersController.register)
 router.post('/logout', usersController.logout)
+router.post('/upload', upload.single('file'), usersController.upload)
 router.get('/:id/articles', usersController.getUserArticles)
 
 module.exports = router
